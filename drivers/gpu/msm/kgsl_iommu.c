313d04544921b117dd142efb7c4ee5639c41fc13
PIPIPIG233666 <pig.priv@gmail.com>
2020-08-25 18:44:51 -0400

    Revert "msm: kgsl: Mark the scratch buffer as privileged"
    Breaks boot.
    
    Log:
    0     0 F kgsl-3d0: |kgsl_iommu_fault_handler| GPU PAGE FAULT: addr = FCC9B000 pid= 0 name=unknown
    0     0 F kgsl-3d0: |kgsl_iommu_fault_handler| context=gfx3d_user TTBR0=0x40001df9e2000 CIDR=0x0 (write permission fault)
    0     0 F kgsl-3d0: |kgsl_iommu_fault_handler| FAULTING BLOCK: CP
    
    This reverts commit 832a86e57e2445241132a8cdb5a80c1d49a92e80.
    
    Change-Id: I4a09a845d8b65ffd764e8f5fa80cde1bc2b70f4c

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -814,6 +814,7 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 	unsigned int no_page_fault_log = 0;
 	char *fault_type = "unknown";
 	struct kgsl_process_private *private;
+	bool fault_ret_flag = false;
 
 	static DEFINE_RATELIMIT_STATE(_rs,
 					DEFAULT_RATELIMIT_INTERVAL,
@@ -861,9 +862,11 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 		 * Turn off GPU IRQ so we don't get faults from it too.
 		 * The device mutex must be held to change power state
 		 */
-		mutex_lock(&device->mutex);
-		kgsl_pwrctrl_change_state(device, KGSL_STATE_AWARE);
-		mutex_unlock(&device->mutex);
+		if (mutex_trylock(&device->mutex)) {
+			kgsl_pwrctrl_change_state(device, KGSL_STATE_AWARE);
+			mutex_unlock(&device->mutex);
+		} else
+			fault_ret_flag = true;
 	}
 
 	contextidr = KGSL_IOMMU_GET_CTX_REG(ctx, CONTEXTIDR);
@@ -931,7 +934,7 @@ static int kgsl_iommu_fault_handler(struct iommu_domain *domain,
 	 * that has faulted, this is better for debugging as it will stall
 	 * the GPU and trigger a snapshot. Return EBUSY error.
 	 */
-	if (test_bit(KGSL_FT_PAGEFAULT_GPUHALT_ENABLE,
+	if (!fault_ret_flag && test_bit(KGSL_FT_PAGEFAULT_GPUHALT_ENABLE,
 		&adreno_dev->ft_pf_policy) &&
 		(flags & IOMMU_FAULT_TRANSACTION_STALLED)) {
 		uint32_t sctlr_val;
